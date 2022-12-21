e50868403e7fe655ec41f6d07ee499ad4535b022
Nguyễn Quang Gia Thuận <GiaThuanNguyen213@gmail.com>
2022-12-21 18:41:43 +0700

    Add Scripts Folder

---
+++ scripts/init.sh
@@ -0,0 +1,16 @@
+while getopts u:a:f: flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        a) age=${OPTARG};;
+        f) fullname=${OPTARG};;
+    esac
+done
+echo "Username: $username";
+echo "Age: $age";
+echo "Full Name: $fullname";
+# echo "Installing";
+# npm install
+# echo "Installed Successfully";
+# echo "Starting";
+# npm run dev
