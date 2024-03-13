de492845b5cb32ada405f219c3eec96ce3e77769
Nguyễn Quang Gia Thuận <GiaThuanNguyen213@gmail.com>
2024-03-14 00:22:46 +0700

    Add pusher -> Test Done

--- pages/direct/inbox/index.tsx
+++ pages/direct/inbox/index.tsx
@@ -1,11 +1,41 @@
 import React from "react";
 import { MainLayout } from "../../../src/Layouts";
+import Pusher from "pusher-js";
+
 
 function InboxPage() {
+  React.useEffect(()=>{
+    var pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY as string, {
+      cluster: 'ap1'
+    });
+    var channel = pusher.subscribe('chat');
+    channel.bind('my-event', function (data: any) {
+      alert(JSON.stringify(data));
+    });
+
+    channel.bind('my-event1', function (data: any) {
+      alert(JSON.stringify(data) + "123123");
+    });
+    return () => {
+      pusher.unsubscribe("chat");
+    };
+  },[])
+
+  // var pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY as string, {
+  //   cluster: 'ap1'
+  // });
+  // var channel = pusher.subscribe('chat');
+  // channel.bind('my-event', function (data: any) {
+  //   alert(JSON.stringify(data));
+  // });
+
+  // channel.bind('my-event1', function (data: any) {
+  //   alert(JSON.stringify(data) + "123123");
+  // });
   return (
     <>
       <MainLayout>
-        
+
       </MainLayout>
     </>
   );
